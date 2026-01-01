99aef277aa1fb567680c0ef4a5a8132758dfa815
Krishna-27 <144887828+Krishna-731@users.noreply.github.com>
2026-01-01 20:20:45 +0530

    Time: 0 ms (100.00%) | Memory: 52 MB (27.26%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -1,14 +1,38 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
-        for(int[] x : matrix) {
-            for(int y : x) {
-                queue.add(y);
-                if (queue.size() > k) {
-                    queue.poll();
-                }
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int low = matrix[0][0];
+        int high = matrix[n-1][m-1];
+        while (low < high) {
+            int mid = low + (high-low)/2;
+            if (helper(matrix, mid) >= k) {
+                high = mid;
+            }
+            else {
+                low = mid + 1;
             }
         }
-        return queue.poll();
+
+        return high;
+    }
+
+    private int helper(int[][] matrix, int mid) {
+        int n = matrix.length;
+        int ans = 0;
+        int row = n-1;
+        int col = 0;
+
+        while (row >= 0 && col < n) {
+            if (matrix[row][col] <= mid) {
+                ans += (row+1);
+                col++;
+            }
+            else {
+                row--;
+            }
+        }
+
+        return ans;
     }
 }
