616229dcfe4612cf8fbafd31957951ee878c693f
Krishna-27 <144887828+Krishna-731@users.noreply.github.com>
2026-02-07 21:36:30 +0530

    Time: 23 ms (57.89%) | Memory: 47.7 MB (75.49%) - LeetSync

---
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumDeletions(String s) {
+        int result = 0;
+        int b = 0;
+        
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'b') {
+                b++;
+            }
+            else if (b > 0) {
+                result++;
+                b--;
+            }
+        }
+        return result;
+    }
+}
